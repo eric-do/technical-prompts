b5b40c55efea6b62d7d0c6b3b37809c2803f6291
Eric Do <ericdo.617@gmail.com>
2021-06-10 10:41:15 -0700

    Refactor to one method

--- golden_ticket/solution.py
+++ golden_ticket/solution.py
@@ -1,59 +1,40 @@
 class Solution:
-    # I:
-    #  - ppc: integer
-    #  - wallet: integer
-    # O:
-    #  - total choclates: integer
-    # C:
-    #  - none
-    # E:
-    #  - invalid inputs? characters instead of numbers
     def exchange_money_for_chocolate(self, ppc, wpc, wallet) -> int:
-        if ppc < 0:
-            raise Exception('Price per chocolate cannot be negative')
+        if ppc <= 0:
+            return 0
 
-        total_chocolates = self.get_chocolates_based_on_wallet(ppc, wallet)
-        remaining_chocolates = total_chocolates
-        while True:
-            chocolates_from_wrappers = self.get_chocolates_based_on_wrappers(wpc, remaining_chocolates)
+        total_chocolates = self.exchange_currency_for_chocolate(ppc, wallet)
+        remaining_chocolates = chocolates_from_wrappers = total_chocolates
+
+        while chocolates_from_wrappers > 0:
+            chocolates_from_wrappers = self.exchange_currency_for_chocolate(wpc, remaining_chocolates)
             remaining_chocolates -= chocolates_from_wrappers * wpc
-            remaining_chocolates += + chocolates_from_wrappers
+            remaining_chocolates += chocolates_from_wrappers
             total_chocolates += chocolates_from_wrappers
-            if chocolates_from_wrappers == 0:
-                break
 
         return total_chocolates
 
-    def get_chocolates_based_on_wallet(self, ppc: int,  wallet: int) -> int:
-        if wallet < ppc:
+    def exchange_currency_for_chocolate(self, units_per_chocolate: int,  total_units: int) -> int:
+        if total_units < units_per_chocolate:
             return 0
 
-        return 1 + self.get_chocolates_based_on_wallet(ppc, wallet - ppc)
+        return 1 + self.exchange_currency_for_chocolate(units_per_chocolate, total_units - units_per_chocolate)
 
-    def get_chocolates_based_on_wrappers(self, wpc, chocolates) -> int:
-        if chocolates < wpc:
-            return 0
-        return 1 + self.get_chocolates_based_on_wrappers(wpc, chocolates - wpc)
 
-    def assert_equals(self, actual: int, expected: int) -> None:
+    def assert_equals(self, actual: int, expected: int, description='No description') -> None:
         if actual == expected:
-            print('PASS')
+            print('PASS [', description, ']')
         else:
-            print('FAIL: expected ', actual, ' to equal ', expected)
-
-    def assert_throws_error(self, func, ppc, wpc, wallet) -> None:
-        try:
-            func(ppc, wpc, wallet)
-            print('FAIL: did not raise exception for invalid input')
-        except Exception:
-            print('PASS')
+            print('PASS [', description, ']: expected', actual, ' to equal ', expected)
 
     def test(self) -> None:
-        self.assert_equals(self.exchange_money_for_chocolate(3, 5, 15), 6)
-        self.assert_equals(self.exchange_money_for_chocolate(3, 2, 10), 5)
-        self.assert_equals(self.exchange_money_for_chocolate(1, 3, 15), 22)
-        self.assert_throws_error(self.exchange_money_for_chocolate, -3, 5, 10)
-        self.assert_equals(self.get_chocolates_based_on_wrappers(3, 15), 5)
+        self.assert_equals(self.exchange_money_for_chocolate(3, 5, 15), 6, "It should return the correct amount when % 0")
+        self.assert_equals(self.exchange_money_for_chocolate(1, 3, 15), 22, "It should return the correct amount when % 0")
+        self.assert_equals(self.exchange_money_for_chocolate(3, 2, 10), 5, "It should return the correct amount when % > 0")
+        self.assert_equals(self.exchange_money_for_chocolate(-3, 5, 10), 0, "It should return 0 if negative price entered")
+        self.assert_equals(self.exchange_money_for_chocolate(2, 5, 1), 0, "It should return 0 user does not have enough money")
+        self.assert_equals(self.exchange_money_for_chocolate(0, 5, 1), 0, "It should return 0 if $0/chocolate")
+        self.assert_equals(self.exchange_currency_for_chocolate(3, 15), 5, "It should return the correct amount")
 
 
 solution = Solution()
