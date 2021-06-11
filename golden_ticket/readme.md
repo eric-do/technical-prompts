97478a551f29305417d1cd03f39effdafee7914d
Eric Do <ericdo.617@gmail.com>
2021-06-11 11:16:47 -0700

    Implement working solution with passing tests

--- golden_ticket/readme.md
+++ golden_ticket/readme.md
@@ -4,10 +4,25 @@ There is a candy shop that sells chocolate so fancy, they are sold in golden wra
 
 Write a function that calculates how many chocolates you are able to eat, based on a given price per choclate and how much money you have.
 
+#1: $15, $3/chocolate = 5 chocolates
+#2: $10, $3/chocolate = 3 chocolates
+
 ## Step 2: Add wrapper exchange for more chocolates
 Because of fancy gold wrappers, the store now takes empty wrappers in exchange for more chocolate. But customers eat the chocolates in the store and exchange immediately instead.
 
 Given a number of wrappers per chocolate, update the function to include chocolates for exchange wrappers.
+
+#1 $20, $3/chocolate, 5 wrappers/chocolate
+// Buy 6 initially, exchange 5 -> 1  = 6 + 1
+= 7
+
+#2 $16, $2/chocolate, 2 wrappers/chocolate
+// Buy 8 initially, exchange 8 -> 4, exchange 2 -> 1 = 8 + 4 + 2 + 1
+= 15
+
+#4 $15, $1/chocolate, 3 wrappers/chocolate
+// Buy 15 initially, exchange 15 -> 5, exchange 5 -> 1 (2 remain), exchange 2 + 1 ->  = 15 + 5 + 1 + 1
+= 22
 
 ## Part 3: Test Cases
 What test should be added to ensure correctness? Candidate should come up with some of these on their own.
--- golden_ticket/solution.py
+++ golden_ticket/solution.py
@@ -3,33 +3,30 @@ class Solution:
         if ppc <= 0:
             return 0
 
-        self.ppc = ppc
-        self.wpc = wpc
-
-        total_chocolates = self.exchange_currency_for_chocolate(ppc, wallet)
-
-        return total_chocolates
+        wrappers = self.exchange_currency_for_chocolate(ppc, wallet)
+        total = wrappers
+        while wrappers >= wpc:
+            exchanged_chocolates = self.exchange_currency_for_chocolate(wpc, wrappers)
+            total += exchanged_chocolates
+            wrappers = wrappers - exchanged_chocolates * wpc + exchanged_chocolates
+        return total
 
     def exchange_currency_for_chocolate(self, units_per_chocolate: int,  total_units: int) -> int:
         if total_units < units_per_chocolate:
             return 0
 
-        chocolates = self.exchange_currency_for_chocolate(units_per_chocolate, total_units - units_per_chocolate)
-        additional_chocolates = self.exchange_currency_for_chocolate(self.wpc, chocolates)
-
-        return chocolates + additional_chocolates
-
+        return 1 + self.exchange_currency_for_chocolate(units_per_chocolate, total_units - units_per_chocolate)
 
     def assert_equals(self, actual: int, expected: int, description='No description') -> None:
         if actual == expected:
             print('PASS [', description, ']')
         else:
-            print('PASS [', description, ']: expected', actual, ' to equal ', expected)
+            print('FAIL [', description, ']: expected', expected, ' but got ', actual)
 
     def test(self) -> None:
-        self.assert_equals(self.exchange_money_for_chocolate(3, 5, 15), 6, "It should return the correct amount when % 0")
-        self.assert_equals(self.exchange_money_for_chocolate(1, 3, 15), 22, "It should return the correct amount when % 0")
-        self.assert_equals(self.exchange_money_for_chocolate(3, 2, 10), 5, "It should return the correct amount when % > 0")
+        self.assert_equals(self.exchange_money_for_chocolate(3, 5, 20), 7, "It should return the correct amount")
+        self.assert_equals(self.exchange_money_for_chocolate(2, 2, 16), 15, "It should return the correct amount")
+        self.assert_equals(self.exchange_money_for_chocolate(1, 3, 15), 22, "It should return the correct amount when there are leftover chocolates")
         self.assert_equals(self.exchange_money_for_chocolate(-3, 5, 10), 0, "It should return 0 if negative price entered")
         self.assert_equals(self.exchange_money_for_chocolate(2, 5, 1), 0, "It should return 0 user does not have enough money")
         self.assert_equals(self.exchange_money_for_chocolate(0, 5, 1), 0, "It should return 0 if $0/chocolate")
